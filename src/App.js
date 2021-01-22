1e70a03077c70145c972cde9e1345a2a976d0178
Tylerve9 <tyler.ve@gmail.com>
2021-01-21 23:48:27 -0400

    Inicio del proyecto frontend base con React y Tailwindcss Base sin contenido solo un componente Navbar de prueba con estilos a manera de prueba

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/navigation/navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="h-20 w-full bg-purple-900 rounded-b-xl shadow-xl">
+        <Navbar />
+        
       </header>
+
+      
+      <div className="flex">
+        <div className="w-20 h-auto border border-black ">Menu lateral</div>
+        <div className="w-4 flex-auto flex h-screen border border-r-black justify-center">
+
+          <div className="w-48 h-20 bg-red-900 text-white text-bold rounded-2xl shadow-lg mt-20 items-center">
+            <p className="justify-center py-4">Contenido de los componentes</p>
+          </div>
+        </div>
+        
+      </div>
     </div>
   );
 }
